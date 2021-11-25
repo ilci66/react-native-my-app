7e136444d737e4aa4f97ae4cf56e33b1ea1c81f8
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-25 09:38:25 +0100

    creating backend

---
+++ backend/shemas/notes.js
@@ -0,0 +1,16 @@
+import {
+  gql
+} from 'apollo-server-express';
+
+// don't forget the "!" means non-nullable
+export default gql `
+  extend type Query {
+    notes: [Note!]
+    note(id: ID!): Note!
+  }
+
+  type Note {
+    id: ID!
+    text: String!
+  }
+`;
