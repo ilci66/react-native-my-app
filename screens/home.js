0735daec9c7bb522bc3579a59a705335ba621b92
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-26 10:35:01 +0100

    working on requests

--- screens/home.js
+++ screens/home.js
@@ -2,12 +2,34 @@ import React from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 
 const Home = ({ navigation }) => {
+
+  const getReq = () => {
+    console.log("sending get request")
+    // it's probably failing because of the ssl 
+    fetch('http://localhost:3001/', )
+    // fetch('https://jsonplaceholder.typicode.com/posts',)
+      .then(response => {
+        // console.log("response",response)
+        // return response.text();
+        return response.json();
+      })
+      .then(data => {
+        console.log("data ==>",typeof data,data.slice(0,2))
+        // setMerchants(data);
+      })
+      .catch(error => console.log(error))
+  }
+
   return (
     <View>
       <Text>This is the home screen</Text>
       <Button
         onPress={() => navigation.navigate('About')}
         title="Go to About"
+      />
+      <Button 
+        onPress={() => getReq()}
+        title="Make a get request"
       />
     </View>
   )
