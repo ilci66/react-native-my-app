9da17921ef456df318b23adcd8e2602c26fd12c9
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-26 09:31:07 +0100

    postgre connection done

---
+++ backend/models/merchant_model.js
@@ -0,0 +1,60 @@
+// now the environment variables work properly
+import dotenv from 'dotenv';
+dotenv.config();
+
+import toPool from 'pg';
+let Pool = toPool.Pool;
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.POSTGRESQL_PORT,
+});
+
+// Looks a little strange, gotta understand why
+
+export const getMerchants = () => {
+  return new Promise(function(resolve, reject) {
+    // they say using * is dangerous for couple of reasons but as I don't have entries yet this should be cool
+    pool.query('SELECT * FROM merchants ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      console.log("results ==>", typeof results)
+      resolve(results.rows);
+    })
+  }) 
+}
+export const createMerchant = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { name, email } = body
+    pool.query('INSERT INTO merchants (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new merchant has been added added: ${results.rows[0]}`)
+    })
+  })
+}
+// commented the faulty code, leaving as a reminder to myself
+// export const deleteMerchant = () => {
+export const deleteMerchant = (id) => {
+  return new Promise(function(resolve, reject) {
+    // const id = parseInt(request.params.id)
+    pool.query('DELETE FROM merchants WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`Merchant deleted with ID: ${id}`)
+    })
+  })
+}
+
+// 
+// module.exports = {
+//   getMerchants,
+//   createMerchant,
+//   deleteMerchant,
+// }
---
+++ backend/server.js
@@ -0,0 +1,81 @@
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const merchant_model = require('./models/merchant_model');
+import {createMerchant, deleteMerchant, getMerchants} from './models/merchant_model.js';
+
+// import {
+//     ApolloServer
+// } from 'apollo-server-express';
+
+const app = express();
+
+app.use(cors({
+  // origin: "http://localhost:3000",
+  // methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  // allowedHeaders: ['Content-Type', 'Authorization', "Access-Control-Request-Headers"]
+}));
+
+app.use(express.json());
+
+// well I had cors
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+//   next();
+// });
+
+// const server = new ApolloServer({});
+
+// server.applyMiddleware({
+//     app,
+//     path: '/graphql'
+// });
+
+// app.listen({
+//     port: 8000
+// }, () => {
+//     console.log('Apollo Server on http://localhost:8000/graphql');
+// });
+
+const port = process.env.PORT;
+
+// create a routes folder for these later maybe, cluttering here is not athe best way to do it
+
+app.get('/', (req, res) => {
+ getMerchants()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+});
+
+app.post('/merchants', (req, res) => {
+  createMerchant(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+});
+
+app.delete('/merchants/:id', (req, res) => {
+ deleteMerchant(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+});
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
