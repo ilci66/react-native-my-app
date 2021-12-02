bde681d4404b7562feb439236cfbf3d31f5583a9
ilci66 <akbiyik.ilker7@gmail.com>
2021-12-02 11:57:26 +0100

    return to basics

--- backend/routes/drawings.js
+++ backend/routes/drawings.js
@@ -1,7 +1,6 @@
 import db from '../db/index.js';
 import express from 'express';
 const router = express.Router();
-
 // Example usage
 
 // const db = require('../db')
@@ -17,19 +16,22 @@ const router = express.Router();
 
 
 
-router.get('/drawings', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // I know this is not even close to ideal but just starting
   console.log("a req came")
-  db.query('SELECT * FROM mock_drawing', [], (err, result) => {
-    if (err) {
-      console.log("error in get all => ",err)
-      return next(err)
-    }
-    console.log("the result in get all ==> ", result)
-    res.send(result.rows[0])
-  })
+  console.log(db.query())
+  const client = await db.getClient();
+  
+  // db.query('SELECT * FROM mock_drawing', ["stuff"], (err, result) => {
+  //   if (err) {
+  //     console.log("error in get all => ",err)
+  //     return next(err)
+  //   }
+  //   console.log("the result in get all ==> ", result)
+  //   res.send()
+  // })
 })
 
-
+// router.post('/')
 
 export default router;
